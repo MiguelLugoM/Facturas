01c07172cd84aeabfe357d8212d7a8f6a96f31ce
Miguel Lugo <miguellugo315651@correo.itm.edu.co>
2024-09-24 17:23:06 -0500

    Empece a crear la Interfaz y la clase generica para el CRUD

---
+++ CapaAccesoDatos/IRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Esta interfaz nos sirve para no repetir codigo e implementarlas en todas las clases que necesiten CRUD
+namespace TallerEvaluativo.CapaAccesoDatos
+{
+    public interface IRepositorio<Entidad> where Entidad : class
+    {
+        // Task significa que el metodo va a ser asincrono y esto mejorara el rendimiento
+        Task<IEnumerable<Entidad>> ReadAll();
+        Task<Entidad> ReadById(int id);
+        Task Create(Entidad entity);
+        Task Update(Entidad entity);
+        Task Delete(int id);
+
+    }
+}
---
+++ CapaAccesoDatos/Repositorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Esto es una clase generica donde vamos  a implementar la interfaz 
+namespace TallerEvaluativo.CapaAccesoDatos
+{
+    internal class Repositorio
+    {
+    }
+}
